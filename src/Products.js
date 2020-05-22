77573fd8ecac42408338aeea101663683e01519c
= <willyamvinicius1900@gmail.com>
2020-05-21 23:39:16 -0300

    added itens

---
+++ src/Products.js
@@ -0,0 +1,16 @@
+import iphoneImage01 from './assets/products/iphone_8_plus/iphone_8_plus.png'
+import iphoneImage02 from './assets/products/iphone_8_plus/iphone_8_plus_front.png'
+import iphoneImage03 from './assets/products/iphone_8_plus/iphone_8_plus_back.png'
+import iphoneImage04 from './assets/products/iphone_8_plus/iphone_8_plus_side.png'
+
+export const categoriesList = [
+    {
+        produto: "Iphone 8 Plus",
+        sku: "2024711194343",
+        price: 649,
+        image01: iphoneImage01,
+        image02: iphoneImage02,
+        image03: iphoneImage03,
+        image04: iphoneImage04,
+    }
+];
